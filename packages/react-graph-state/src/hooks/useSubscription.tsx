0868a4d8b0dfb09b097b7f1c17aa366d91320727
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-12-13 21:39:08 +0800

    Write through state when dependencies change

--- packages/react-graph-state/src/hooks/useSubscription.tsx
+++ packages/react-graph-state/src/hooks/useSubscription.tsx
@@ -162,6 +162,7 @@ export default function useSubscription<T>({
     && prevShouldUpdate !== shouldUpdate
   ) {
     currentValue = read();
+    state.current = currentValue;
   }
 
   useDebugValue(currentValue);
