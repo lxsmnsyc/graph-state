8aab58464d7d95fb9499cd12d2c7f13bf66f7e94
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-12-20 16:07:37 +0800

    Update dependencies

--- packages/re-graph-state/src/ReGraphState.bs.js
+++ packages/re-graph-state/src/ReGraphState.bs.js
@@ -1,19 +1,7 @@
 'use strict';
 
-var GraphState = require("graph-state");
-var ReactGraphState = require("react-graph-state");
 
 var GraphDomain = {};
 
-var node = GraphState.createGraphNode({
-      get: (function (param) {
-          return 0;
-        }),
-      key: undefined
-    });
-
-ReactGraphState.useGraphNodeHydrate(node, 1);
-
 exports.GraphDomain = GraphDomain;
-exports.node = node;
-/* node Not a pure module */
+/* No side effect */
