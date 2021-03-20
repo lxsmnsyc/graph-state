806db64b528c504dc1d123be0dec04a85e5db253
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-03-20 15:08:23 +0800

    Update Dispatch type

--- packages/preact-graph-state/src/hooks/useGraphNodeDispatchBase.tsx
+++ packages/preact-graph-state/src/hooks/useGraphNodeDispatchBase.tsx
@@ -31,7 +31,7 @@ import {
 } from '@lyonph/preact-hooks';
 import { compareArray } from '../utils/compareTuple';
 
-export type GraphNodeDispatch<A> = (action: A) => void;
+export type GraphNodeDispatch<A> = (action: A) => (void | Promise<void>);
 
 export default function useGraphNodeDispatchBase<S, A>(
   memory: GraphDomainMemory,
